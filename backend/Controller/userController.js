b3c26a50f6afad950164f79995bc3c2b6037d6ae
Mahmudul Hasan <hasan22dev@gmail.com>
2022-09-13 18:14:34 +0600

    user activation and login

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -1,5 +1,7 @@
 const User = require("../Models/UserModels");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const {
   emailValidation,
   lengthValidation,
@@ -7,8 +9,8 @@ const {
 } = require("../Helpers/Validation");
 const { generateToken } = require("../Helpers/Tokens");
 const { sendVerificationEmail } = require("../Helpers/Mailer");
-// const { sendEmailVerification } = require("../Helpers/Mailer");
 
+// REGISTRATION
 exports.register = async (req, res) => {
   try {
     const {
@@ -75,15 +77,78 @@ exports.register = async (req, res) => {
       gender,
     });
 
+    await newUser.save();
+
     const emailVerificationToken = generateToken({ id: User._id }, "30m");
 
+    // SENT MAIL FOR VERIFY USER REGISTRATION
     const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
-
     sendVerificationEmail(newUser.email, newUser.firstName, url);
 
-    await newUser.save();
-    res.send(newUser);
+    // TOKEN FOR USE LOGIN
+    const token = generateToken({ id: newUser._id.toString() }, "3d");
+
+    res.send({
+      id: newUser._id,
+      userName: newUser.userName,
+      firstName: newUser.firstName,
+      lastName: newUser.lastName,
+      token: token,
+      varified: newUser.varified,
+      message: "Registration successful ! Verify your mail for login",
+    });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ massage: error.massage });
+  }
+};
+
+// USER ACTIVATION AFTER VERIFY EMAIL
+exports.userActivation = async (req, res) => {
+  try {
+    const { token } = req.body;
+
+    const user = jwt.verify(token, process.env.TOKEN_SECRET);
+    const check = await User.findById(user.id);
+
+    if (check.varified == true) {
+      return res.status(400).json({ massage: "This email already activated" });
+    } else {
+      await User.findByIdAndUpdate(user.id, { varified: true });
+      return res.status(200).json({ massage: "Account has been activated" });
+    }
+  } catch (error) {
+    res.status(500).json({ massage: error.massage });
+  }
+};
+
+// LOGIN
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ massage: "Incorrect Email Address, try again" });
+    }
+
+    const checkPassword = bcrypt.compareSync(password, user.password);
+    if (!checkPassword) {
+      res.status(500).json({ massage: "Invalid Password" });
+    } else {
+      const token = generateToken({ id: user._id.toString() }, "3d");
+      res.send({
+        id: user._id,
+        userName: user.userName,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        token: token,
+        varified: user.varified,
+        message: "Login successful",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ massage: error.massage });
   }
 };
--- backend/Routes/authRouter.js
+++ backend/Routes/authRouter.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { register } = require("../Controller/userController");
+const {
+  register,
+  userActivation,
+  login,
+} = require("../Controller/userController");
 
 const authRoute = express.Router();
 
 authRoute.post("/register", register);
+authRoute.post("/activate", userActivation);
+authRoute.post("/login", login);
 
 module.exports = authRoute;
